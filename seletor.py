51d6ce436d90017244e434a827f76deee881cd48
Kennedy247 <00001118706171sp@al.educacao.sp.gov.br>
2026-05-04 09:33:21 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,9 @@
+#--seletor de caracteres--
+letra = input("digite a letra")
+if letra == "a":
+  print("vc escolheu a letra a")
+elif letra == "b":
+  print("vc escolheu a letra b")
+else:
+  print("essa opcao n esta disponivel")
+  
